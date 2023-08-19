c4d890034f066990b13831552319e02aad647d56
JasonGrass <junjiegrass@qq.com>
2023-08-19 20:19:24 +0800

    更新部署的仓库

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "JasonGrass", // Usually your GitHub org/user name.
-  projectName: "auto-extension-manager", // Usually your repo name.
+  projectName: "extension-manager-docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
